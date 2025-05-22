4c459d6e9efabfa8bea599bc7bcf223efea04624
Beatriz Alves <55098795+bea3853@users.noreply.github.com>
2025-05-22 17:42:27 -0300

    Add files via upload

---
+++ atividade1.py
@@ -0,0 +1,68 @@
+# comentário
+# dados primitivos
+# para carregar o código, clique em f5
+# strings = textos(str)
+# função print ,mostra textos na tela
+
+print('Olá mundo!')
+print('Sejam bem vindos', 'Pessoas')
+print('Olá ', 'meu nome é', 'Fernando')
+print('--------------')
+
+# dado do tipo inteiro(int)
+
+print(1)
+print(-30)
+print(0)
+
+# dado do tipo real (float)
+print(1.5)
+print(10.0)
+print(30.5)
+print(.5)
+print(0.)
+
+
+# dado do tipo boleano(bool)
+
+print(True)
+print(False)
+
+
+# input() =  guardar um dado
+
+
+# variáveis :
+
+nome  =  'Beatriz'
+idade =  25
+endereco  = 'Rua 25, Continental'
+curso = 'Programação em  Python'
+
+print('Olá meu nome é', nome)
+print('minha idade é ', idade)
+print('Minha casa está no endereço', endereco)
+print('Faço o curso', curso)
+
+
+# calculadora de soma
+
+numero_1 = int(input('digite um número'))
+numero_2 = int(input('Outro número'))
+
+print(numero_1 * numero_2)
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ atividade2.py
@@ -0,0 +1,10 @@
+# soma de  2 fluxograma 
+# 4 funções básicas
+
+n1 = float(input('Numero 1 :'))
+n2 = float(input('Numero 2 :'))
+
+print('Resultado soma', n1 +  n2)
+print('Resultado subtração ', n1 -  n2)
+print('Resultado multiplicação', n1 *  n2)
+print('Resultado divisão ', n1 /  n2)
