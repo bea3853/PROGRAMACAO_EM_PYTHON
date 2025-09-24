50318f66b277fe37eebfba56482ead62118ad2cc
bea3853 <blues5832@gmail.com>
2025-09-24 15:41:35 -0300

    teste 2

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-# Multiplique 4 por 6 e divida o resultado por 2. 
-# (Concatene com a palavra resultado)
+# # Multiplique 4 por 6 e divida o resultado por 2. 
+# # (Concatene com a palavra resultado)
 
 
-multi =  (4 * 6)/2
-print('Resultado', multi,)
+# multi =  (4 * 6)/2
+# print('Resultado', multi)
+
+# Exercícios aula 6: 
+
+soma =  (10 +  10 )/ 2
